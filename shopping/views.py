5990b46cd707096a8a02494f672e394ae6fc2932
Pak Youngrok <pak.youngrok@gmail.com>
2013-09-24 14:39:33 +0900

    @login_required

--- shopping/views.py
+++ shopping/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from shopping.models import Product
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 	products = Product.objects.all()	
@@ -10,5 +11,6 @@ def show(request, resource_id):
 	product = Product.objects.get(id=resource_id)
 	return render_to_response('show.html', locals())
 
+@login_required
 def favorite(request, resource_id):
 	return HttpResponse('favorite %s' % resource_id)
